0996485d9474b67923998bf7fbfc94d961236ae0
ChaoticMagician <ainidetianshi21@163.com>
2018-03-27 14:21:36 +0800

    简单的webpack练习
    简单的webpack练习

---
+++ build/webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path')
+
+module.exports = {
+    entry: path.join(__dirname,'../src/main.js'),
+    output: {
+        filename: 'main.js',
+        path: path.join(__dirname,'../dist')
+    },
+    module: {
+        rules:[
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader'
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.(gif|jpg|jpeg|png|svg)$/,
+                use: [
+                    {
+                        loader: 'url-loader',
+                        options: {
+                            limit: 1024,
+                            name: '[name].[ext]'
+                        }
+                    }
+                ]
+            }
+        ]
+    }
+}
